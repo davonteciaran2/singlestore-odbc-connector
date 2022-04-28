b8b208d6b37aaf98c44e5840e3898be292443b31
Adalbert Makarovych <amakarovych-ua@memsql.com>
2022-04-29 00:21:10 +0300

    check point

--- .circleci/setup-environment-docker.sh
+++ .circleci/setup-environment-docker.sh
@@ -26,7 +26,7 @@ then
   wget https://github.com/Kitware/CMake/releases/download/v3.20.3/cmake-3.20.3-linux-x86_64.tar.gz
   cd /usr || exit
   tar --strip-components=1 -xzf /root/project/cmake-3.20.3-linux-x86_64.tar.gz
-  yum makecache
+  yum makecache --refresh
   yum -y install xdg-utils w3m curl
 else
   apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 648ACFD622F3D138  # Debian 10/buster archive signing key
@@ -38,3 +38,5 @@ else
   apt-get update
   apt-get install -y unixodbc-dev=2.3.6-0.1 xdg-utils w3m curl
 fi
+
+echo 'export BROWSER=$(which w3m)' >> $BASH_ENV
